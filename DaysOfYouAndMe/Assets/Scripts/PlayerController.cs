8b2c7df83afeaedf82ecf91dc77f885218f0359a
George Rashed <goerge1110@gmail.com>
2025-12-24 01:08:33 +0200

    Basic player movement, and BG Music sample implemented.

--- DaysOfYouAndMe/Assets/Scripts/PlayerController.cs
+++ DaysOfYouAndMe/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController _charCon;
+
+    public float moveSpeed = 10;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _charCon = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveAxis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        Vector3 movement = transform.right * moveAxis.x + transform.forward * moveAxis.y;
+
+        _charCon.Move(movement * moveSpeed * Time.deltaTime);
     }
 }
